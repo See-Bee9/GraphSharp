f090779bce745c051ecc5ffe92f202bfd1d054e5
See_Bee <cbird7@vols.utk.edu>
2017-04-19 13:29:13 -0400

    Added some interfaces to be used later.

---
+++ GraphSharp/GraphSharp/GraphSharp/UUGraph.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphSharp
+{
+    /// <summary>
+    /// Unweighted undirected graph
+    /// </summary>
+    public class UUGraph : Dictionary<INode,double>, IGraph
+    {
+        private double[,] m_adjmatrix;
+        public int Order
+        {
+            get
+            {
+                return m_adjmatrix.GetLength(1);
+            }
+            private set { }
+        }
+
+        public UUGraph(int Order)
+        {
+            m_adjmatrix = new double[Order, Order];
+        }
+
+        public IComponent BreadthFirstSearch(INode sink)
+        {
+
+        }
+    }
+}
